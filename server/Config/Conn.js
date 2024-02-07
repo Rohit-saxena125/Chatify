cf14e9505fe23a1cb82fa617b6bf778a95896801
Rohit-saxena125 <rohitsaxena125@gmail.com>
2024-02-08 00:43:02 +0530

    Backend Commit

--- server/Config/Conn.js
+++ server/Config/Conn.js
@@ -1,1 +1,12 @@
-connection
+const mongoose = require('mongoose');
+const dbconnect = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+module.exports = dbconnect;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-console.log('server.js');
+require("dotenv").config("./.env");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+const dbconnect = require("./Config/Conn");
+const errormiddleare = require("./Middleware/Error");
+const AuthRoute = require("./Routes/Auth-route");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/auth", AuthRoute);
+app.use(errormiddleare);
+dbconnect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
